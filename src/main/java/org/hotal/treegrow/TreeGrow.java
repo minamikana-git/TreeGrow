8864a7f51bca6c5ab65d2a394cf32fb51c91bcb3
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-04-01 19:19:29 +0900

    first commit java 17 support 1.19.4 support

--- src/main/java/org/hotal/treegrow/TreeGrow.java
+++ src/main/java/org/hotal/treegrow/TreeGrow.java
@@ -1,17 +1,45 @@
 package org.hotal.treegrow;
 
+import org.bukkit.Material;
+import org.bukkit.TreeType;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TreeGrow extends JavaPlugin {
+public class TreeGrow extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // イベントリスナーを登録
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        // Shiftキーが押されたかをチェック
+        if (!player.isSneaking()) {
+            return;
+        }
+
+        // ブロックがクリックされたかをチェック
+        Block block = event.getClickedBlock();
+        if (block == null) {
+            return;
+        }
+
+        // クリックされたブロックが土でなければ処理を終了
+        if (block.getType() != Material.DIRT && block.getType() != Material.GRASS_BLOCK) {
+            return;
+        }
+
+        // ツリーを成長させる
+        block.getWorld().generateTree(block.getLocation(), TreeType.TREE);
     }
 }
+
+
